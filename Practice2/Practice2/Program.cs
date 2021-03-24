5551f6c38187618b7fb5ece60ea71048cf6198f5
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-03-24 20:35:13 +0200

    added empty project

---
+++ Practice2/Practice2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
